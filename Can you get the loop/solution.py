0969d7da78d722bff0285ad408e446fa3c7454d9
Sofia Trush <trush.pn@ucu.edu.ua>
2025-04-03 23:42:46 +0300

    Add solution for Can you get the loop?

--- Can you get the loop/solution.py
+++ Can you get the loop/solution.py
@@ -1,1 +1,19 @@
+"""Can you get the loop?"""
+class Node:
+    """class for a node"""
+    def __init__(self, next=None):
+        self.next = next
 
+def loop_size(node):
+    """
+    this function determines the length of the loop
+    """
+    visited = {}
+    step = 0
+
+    while node not in visited:
+        visited[node] = step
+        node = node.next
+        step += 1
+    cycle_length = step - visited[node]
+    return cycle_length
