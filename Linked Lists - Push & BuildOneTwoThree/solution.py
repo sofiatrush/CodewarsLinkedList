c388a11747c6107770a7de9f0368e6ac4865c94a
Sofia Trush <trush.pn@ucu.edu.ua>
2025-04-03 23:35:43 +0300

    Add solution to Linked Lists - Push & BuildOneTwoThree

--- Linked Lists - Push & BuildOneTwoThree/solution.py
+++ Linked Lists - Push & BuildOneTwoThree/solution.py
@@ -1,1 +1,32 @@
+"""Linked Lists - Push & BuildOneTwoThree"""
 
+# from preloaded import Node
+
+'''
+Node is defined in preloaded like this:
+'''
+class Node:
+    """
+    Class for Node
+    """
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def push(head, data):
+    """
+    function fot pushing
+    """
+    return Node(data, head)
+
+def build_one_two_three():
+    """
+    The buildOneTwoThree function should create \
+    and return a linked list with three nodes: \
+    1 -> 2 -> 3 -> null
+    """
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
