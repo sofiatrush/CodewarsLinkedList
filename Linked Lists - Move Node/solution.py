7371c77447f6342deccd82a5aef2e01a7ed0a6c0
Sofia Trush <trush.pn@ucu.edu.ua>
2025-04-03 23:31:55 +0300

    Added solution for Linked Lists - Move Node

--- Linked Lists - Move Node/solution.py
+++ Linked Lists - Move Node/solution.py
@@ -1,1 +1,27 @@
+"""Linked Lists - Move Node"""
 
+class Node(object):
+    """
+    Class for a Node
+    """
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    """
+    we use a Context object to store and return the state of the two linked lists.
+    """
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    """
+    function which takes the node from the front of the source \
+    list and moves it to the front of the destintation list.
+    """
+    new_source = source.next
+    source.next = dest
+    new_dest = source
+    return Context(new_source, new_dest)
