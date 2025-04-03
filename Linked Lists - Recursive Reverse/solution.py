6c52479ada885eef5c668047df6e038d97dd8862
Sofia Trush <trush.pn@ucu.edu.ua>
2025-04-03 23:36:26 +0300

    Added solution for Linked Lists - Recursive Reverse

--- Linked Lists - Recursive Reverse/solution.py
+++ Linked Lists - Recursive Reverse/solution.py
@@ -1,1 +1,16 @@
+"""Linked Lists - Recursive Reverse"""
 
+class Node(object):
+    """class for a node"""
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    """function that recursively reverses a linked list."""
+    if head is None or head.next is None:
+        return head
+    rest = reverse(head.next)
+    head.next.next = head
+    head.next = None
+    return rest
