1734a1ca3fac5ac1d6ce73866ef48afbb7a3d5f2
Sofia Trush <trush.pn@ucu.edu.ua>
2025-04-03 23:37:19 +0300

    Added solution for Linked Lists - Remove Duplicates

--- Linked Lists - Remove Duplicates/solution.py
+++ Linked Lists - Remove Duplicates/solution.py
@@ -1,1 +1,19 @@
+"""Linked Lists - Remove Duplicates"""
 
+class Node(object):
+    """class for a node"""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    """
+    function which takes a list sorted in increasing \
+    order and deletes any duplicate nodes from the list."""
+    current = head
+    while current and current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
